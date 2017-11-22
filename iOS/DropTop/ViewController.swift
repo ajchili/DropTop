9be64aeaa57151649d2a32908c3e87598a56954e
Kirin Patel <kirinpatel@gmail.com>
2017-11-22 12:32:02 -0500

    Setup iOS project

---
+++ iOS/DropTop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropTop
+//
+//  Created by Kirin Patel on 11/22/17.
+//  Copyright © 2017 Kirin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
